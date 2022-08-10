4122a9cbab70525aa9047a85c594cf7be6d16387
phollphat <phollaphat.s@ku.th>
2022-08-10 20:58:25 +0700

    updatelogin

--- src/main/java/ku/cs/ProjectApplication.java
+++ src/main/java/ku/cs/ProjectApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class ProjectApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ProjectApplication.class.getResource("project.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ProjectApplication.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Project-Name");
         stage.setScene(scene);
