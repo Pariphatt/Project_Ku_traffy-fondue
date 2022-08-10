758341c18a987ab3b6b70c4f4bcce644f58d992c
Phonburis <phonburis.n@ku.th>
2022-08-10 19:13:09 +0700

    Update Register

--- src/main/java/ku/cs/ProjectApplication.java
+++ src/main/java/ku/cs/ProjectApplication.java
@@ -1,20 +1,19 @@
 package ku.cs;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-
+import com.github.saacsos.FXRouter;
 public class ProjectApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ProjectApplication.class.getResource("project.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("Project-Name");
-        stage.setScene(scene);
-        stage.show();
+       FXRouter.bind(this, stage, "Register", 800, 600);
+        configRoute();
+        FXRouter.goTo("Register");
+    }
+    private static void configRoute() {
+        String packageStr = "ku/cs/";
+        FXRouter.when("Register",packageStr + "Register.fxml");
     }
 
     public static void main(String[] args) {
